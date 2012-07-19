77b39f4f34d3cc0a5f7957d7b91a52b562c44fd6
Brendan Byrd <Perl@ResonatorSoft.org>
2012-07-19 16:05:02 -0400

    Initial commit

---
+++ lib/Dist/Zilla/Role/MetaCPANInterfacer.pm
@@ -0,0 +1,36 @@
+package Dist::Zilla::Role::MetaCPANInterfacer;
+
+# VERSION
+# ABSTRACT: ---insert abstract here---
+
+use sanity;
+
+### INSERT CODE HERE ###
+
+42;
+
+__END__
+
+=begin wikidoc
+
+= SYNOPSIS
+ 
+   # code
+ 
+= DESCRIPTION
+ 
+Insert description here...
+
+= CAVEATS
+
+Bad stuff...
+
+= SEE ALSO
+
+Other modules...
+
+= ACKNOWLEDGEMENTS
+
+Thanks and stuff...
+
+=end wikidoc
